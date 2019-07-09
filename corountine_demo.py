a499d27cae47bfa4a756126d7758b1b2b3201e56
linln1 <41333235+linln1@users.noreply.github.com>
2019-07-09 22:14:08 +0800

    Create corountine_demo.py

---
+++ corountine_demo.py
@@ -0,0 +1,36 @@
+# usr/bin/env python3
+# -*- encoding:utf-8 -*-
+
+import asyncio
+
+@asyncio.coroutine
+def hello():
+      print('Hello, world! (%s)' % threading.currentThread())
+      yield from asyncio.sleep(1)
+      print('Hello, again! (%s)' % threading.currentThread())
+      
+
+loop = asyncio.get_loop_event()
+task = [hello(), hello()]
+loop.run_until_complete(asyncio.wait(tasks))
+loop.close()
+
+
+def getwebpage(host):
+      print('getwebpage %s...' % host)
+      connect = asyncio.open_connection(host, 80)
+      reader, writer = yield from connect
+      header  = 'GET / HTTP/1.0\r\nHost:  %s\r\n\r\n' % host
+      writer.write(header.encode('utf-8')
+      yield from writer.drain()
+      while True:
+            line = yield from reader.readline()
+            if line == b'\r\n':
+                  break
+            print('%s header > %s' % (host, line.decode('utf-8').rstrip())
+      write.close()
+      
+loop = asyncio.get_loop_event()
+tasks = [getwebpage(host) for host in ['www.sina.com.cn', 'www.sohu.com', 'www.163.com']]
+loop.run_until_complete(asyncio.wait(tasks))
+loop.close()
