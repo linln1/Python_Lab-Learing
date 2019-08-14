4254960611ab85208b48a4260882f5c2800c33f5
linln1 <41333235+linln1@users.noreply.github.com>
2019-08-14 21:57:15 +0800

    Update Platt_SMO_demo.py

--- Support_Vector_Machine/Platt_SMO_demo.py
+++ Support_Vector_Machine/Platt_SMO_demo.py
@@ -30,7 +30,7 @@ def clipAlpha(aj, H,L):
         aj=L
     return aj
 
-#SMO函数的伪代码大致如下: 
+#SMO函数的伪代码大致如下:
 # # 创建一个alpha 向量并将其初始化为0向量
 # 当迭代次数小于最大迭代次数时：
 #     对数据集中的每个数据向量：
@@ -63,7 +63,7 @@ def smoSimple(dataMatIn, classLables, C, toler, maxIter):
                  else:
                      L = max(0, alphas[j]+ alphas[i]-C)
                      H = min(C< alphas[i] + alphas[j])
-                 if L==H: print('L=H') ;continue
+                 if L == H: print('L=H');continue
                  eta = 2.0*dataMatrix[i,:]*dataMatrix[j,:].T - dataMatrix[i,:]*dataMatrix[i,:].T-dataMatrix[j,:]*dataMatrix[j,:].T
                  if eta>=0 : print('eta>=0'); continue
                  alphas[j] -= labelMatrix[j]*(Ei-Ej)/eta
@@ -82,12 +82,76 @@ def smoSimple(dataMatIn, classLables, C, toler, maxIter):
          else: iter = 0
          print('iteration number: %d' % iter)
     return b,alphas
-             
-                
+
+
 # 为了得到支持向量的个数:
 # np.shape(alphas[alphas>0])
 # 为了得到那些数据点是支持向量，输入：
 # for i in range(100):
 #     if alphas[i]>0.0: print(dataArr[i], labelArr[i])
 
+class optStruct:
+    def __init(self, dataMatIn , classLabels, C, toler):
+        self.X = dataMatIn
+        self.labelMat = classLabels
+        self.C = C
+        self.tol = np.shape(dataMatIn)[0]
+        self.alphas = np.mat(np.zeros(self.m, 1))
+        self.b = 0
+        self.eCache = np.mat(np.zeros((self.m , 2)))
+
+def calcEk(oS, k):
+    fXk = np.float(np.mutiply(oS.alphas, oS.labelMat).T * (oS.X*oS.X[k,:].T )) + oS.b 
+    Ek = fXk - np.float(oS.labelMat[k])
+    return Ek
+
+def selectJ(i, oS, Ei):
+    maxK = -1; maxDeltaE = 0; Ej = 0
+    oS.eCache[i] = [1,Ei]
+    validEcacheList = np.nonzero(oS.eCache[:,0].A)[0]
+    if (len(validEcacheList)) > 1:
+        for k in validEcacheList:
+            if k == i: continue
+            Ek = calcEk(oS, k)
+            deltaE = abs(Ei-Ek)
+            if (deltaE > maxDeltaE):
+                maxK = k ;maxDeltaE = deltaE; Ej = Ek
+        return maxK, Ej
+    else:
+        j = selectJrand(i, oS.m)
+        Ej = calcEk(oS, j)
+    return j ,Ej
+
+def updateEk(oS, k ):
+    Ek = calcEk(oS, k)
+    oS.eCache[k] = [1,Ek]
     
+def innerL(i, oS):
+    Ei = calcEk(oS, i)
+    if ((oS.labelMat[i]* Ei< -oS.tol ) and (oS.alphas[i] < oS.C)) or ((oS.labelMat[i]*Ei > oS.tol) and (oS.alphas[i] > 0)):
+        j, Ej = selectJ(i, oS, Ei)
+        alphaIold = oS.alphas[i].copy()
+        alphaJold = oS.alphas[j].copy()
+        if (oS.labelMat[i]!= oS.labelMat[j]):
+            L = max(0, oS.alphas[j] - oS.alphas[i])
+            H = min(oS.C, oS.C + oS.alphas[j] -oS.alphas[i])
+        else:
+            L = max(0, oS.alphas[j] + oS.alphas[j] -oS.C)
+            H = min(oS.C , oS.alphas[j] + oS.alphas[i])
+        if L == H: print('L=H'); return 0
+        eta = 2.0* oS.X[i,:]*oS.X[j,:].T - oS.X[i,:]*oS.X[i,:].T - oS.X[j,:]*oS.X[j,:].T
+        if eta>=0 : print('eta>=0') ; return 0
+        oS.alphas[i] -= oS.labelMat[j]*(Ei- Ej)/eta
+        oS.alphas[j] = clipAlpha(oS.alphas[j], H, L)
+        updateEk(oS, j)
+        if (abs(oS.alphas[j] - alphaJold) < 0.00001):
+            print('j not moving enough'); return 0
+        oS.alphas[i] += oS.labelMat[j]*oS.labelMat[i]*(alphaJold - oS.alphas[j])
+        updateEk(oS, i)
+        b1 = oS.b - Ei - oS.labelMat[i]*(oS.alphas[i] - alphaIold)*oS.X[i,:]*oS.X[i,:].T - oS.labelMat[j]*(oS.alphas[j] - alphaJold)*oS.X[i,:]*oS.X[j,:].T
+        b2 = oS.b - Ej - oS.labelMat[i]*(oS.alphas[i] - alphaIold) * oS.X[i, :] * oS.X[i, :].T - oS.labelMat[j] * (oS.alphas[j] - alphaJold) * oS.X[j, :] * oS.X[j, :].T
+        if (0<oS.alphas[i]) and (oS.C >oS.alphas[i]): oS.b = b1
+        elif (0<oS.alphas[j]) and (oS.C > oS.alphas[j]): oS.b = b2
+        else: oS.b = (b1+b2) /2.0
+        return 1 
+    else:return 0
